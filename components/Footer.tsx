4d1100f8e20157976699dc2caffbc2c944fa71db
Bruno Dias <62982430+brndiasdev@users.noreply.github.com>
2024-07-30 20:54:53 -0300

    batman

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
               key={profile.id}
               className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg border border-black-300 bg-black-200 bg-opacity-75 backdrop-blur-lg backdrop-filter"
             >
-              <img src={profile.img} alt={idx} width={20} height={20} />
+              <img src={profile.img} alt={`${idx}`} width={20} height={20} />
             </div>
           ))}
         </div>
